fc4021083d9c23c3f9d0705add8fb312044cbc89
Harry <harry@midnight-labs.org>
2016-02-25 23:57:13 +0000

    Included license in evaluator and added private scope to evaluateFun

--- source/lisp/evaluator.d
+++ source/lisp/evaluator.d
@@ -1,9 +1,28 @@
+/* A minimal Lisp interpreter
+   Copyright 2004 Andru Luvisi
+   Copyright 2015 Harry Roberts
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License , or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, write to the Free Software
+   Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 module lisp.evaluator;
 
 private import lisp.core;
 private import lisp.s11n;
 
-Obj evaluateFun(ref Obj env, Obj_Fun obj, Obj args) pure @safe nothrow {
+private Obj evaluateFun(ref Obj env, Obj_Fun obj, Obj args) pure @safe nothrow {
 	if( obj.func !is null ) {
 		return obj.func(env, args);
 	}
